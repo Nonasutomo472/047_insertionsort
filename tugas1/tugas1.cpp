cb6f3389a966c5aa7ac9f9217b836e147e1011d4
winartinurutami <winarti.nur.ft20@mail.umy.ac.id>
2024-03-14 11:37:58 +0700

    membuat void input

--- tugas1/tugas1.cpp
+++ tugas1/tugas1.cpp
@@ -6,6 +6,34 @@ int a[20];                //Deklarasi array a dengan ukuran 20
 int n;                    //Deklarasi variabel n untuk menyimpan banyaknya elemen pada array
 
 
+void input()
+{            //Procedure untuk input
+    while (true)
+    {        //Looping
+        cout << "Masukan banyaknya elemen pada array : ";      //Output ke layar
+        cin >> n;             //Input dari pengguna
+        if (n <= 20)          //Jika n kurang dari atau sama dengan 20
+            break;            //Keluar dari loop
+        else
+        {                //Jika n lebih dari 20
+            cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";    //Output ke layar
+        }
+
+    }
+    cout << endl;                                                //Output ke barisan kosong
+    cout << "=======================" << endl;                   //Output ke layar
+    cout << "Masukan Eleman Array" << endl;                      //Output ke layar
+    cout << "=======================" << endl;                   //Output ke layar
+
+    for (int i = 0; i < n; i++)
+    {                   //Looping dengan j dimulai dari 0 hingga n-1
+        cout << "Data ke -" << (i + 1) << ": ";     //Output ke layar
+        cin >> a[i];                                //Input dari pengguna
+    }
+}
+
+
+
 int main()
 {
 
