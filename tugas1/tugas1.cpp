00ce64693f28304a90acce08e64a3b58ea32e8a3
winartinurutami <winarti.nur.ft20@mail.umy.ac.id>
2024-03-14 11:36:37 +0700

    Add project files.

---
+++ tugas1/tugas1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+int a[20];                //Deklarasi array a dengan ukuran 20
+int n;                    //Deklarasi variabel n untuk menyimpan banyaknya elemen pada array
+
+
+int main()
+{
+
+        return 0;
+    
+}
